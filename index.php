efe4cfa13e92fd63fe49cc435a76e1a27b06012c
Guillaume Boudreau <guillaume@2xminteractive.com>
2013-04-02 12:00:58 -0400

    New link to Chrome extension by Google, now that it's back in the App Store.

--- index.php
+++ index.php
@@ -146,12 +146,12 @@ function gen_uuid() {
 <br/><br/>
 <?php if (isset($uuid)): ?>
     <div>
-        Tip: Use this secret URL to come back to your account later:<br/>
+        Important: Use this secret URL to come back to your account later:<br/>
         &nbsp; <a href="http://rss-for-later.pommepause.com/?uuid=<?php echo $uuid ?>">http://rss-for-later.pommepause.com/?uuid=<?php echo $uuid ?></a>
     </div>
     <br/>
     <div>
-        More Tips: If you have the Chrome <a href="https://chrome.google.com/webstore/detail/rss-subscription-extensio/bmjffnfcokiodbeiamclanljnaheeoke" target="_blank">RSS Subscription Extension</a> installed (or an equivalent extension in other browsers), you can subscribe to feeds using it by configuring the following URL:<br/>
+        Tip: If you have the Chrome <a href="https://chrome.google.com/webstore/detail/rss-subscription-extensio/nlbjncdgjeocebhnmkbbbdekmmmcbfjd" target="_blank">RSS Subscription Extension (by Google)</a> installed (or an equivalent extension in other browsers), you can subscribe to feeds using it by configuring the following URL:<br/>
         &nbsp; http://rss-for-later.pommepause.com/?uuid=<?php echo $uuid ?>&subscribe=%s<br/>
     </div>
 <?php else: ?>
