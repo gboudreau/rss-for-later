d10b9f37b986f4b771f9df398f30c1daf1f920bb
Guillaume Boudreau <github@danslereseau.com>
2014-06-29 21:19:20 -0400

    Give higher priority to older users, when fetching Twitter and RSS feeds

--- cron/index.php
+++ cron/index.php
@@ -9,13 +9,13 @@ if (isset($uuid)) {
     exit();
 }
 
-$q = "SELECT uuid FROM users WHERE twitter_access_token IS NOT NULL";
+$q = "SELECT uuid FROM users WHERE twitter_access_token IS NOT NULL ORDER BY id";
 $uuids = DB::getAllValues($q);
 foreach ($uuids as $uuid) {
     Twitter::downloadTimeline($uuid);
 }
 
-$q = "SELECT uuid FROM users WHERE pocket_access_token IS NOT NULL";
+$q = "SELECT uuid FROM users WHERE pocket_access_token IS NOT NULL ORDER BY id";
 $uuids = DB::getAllValues($q);
 foreach ($uuids as $uuid) {
     RSS::downloadRSS($uuid);
